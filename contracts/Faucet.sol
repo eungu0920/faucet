35aab192ba2816dbf0747a6a3ed9dcc930b4ca61
Gray <eungu0920@inu.ac.kr>
2024-06-27 17:28:35 +0900

    Add comments to Faucet contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.20;
 
 import "./utils/SafeERC20.sol";
 
+/// @title Owned Contract
+/// @notice This contract sets the deployer as the owner
 contract owned {
     address public owner;
 
@@ -16,6 +18,8 @@ contract owned {
     }
 }
 
+/// @title Faucet Contract
+/// @notice This contract allows users to request tokens with a time limit
 contract Faucet is owned {
     using SafeERC20 for IERC20;
     uint256 public timeLimit;
@@ -30,6 +34,13 @@ contract Faucet is owned {
     event WithdrawalRequest(address indexed to, IERC20 token ,uint amount);
     event Deposit(address indexed from, uint amount);
 
+    /// @notice Initializes the contract with the given parameters
+    /// @param _TON The address of the TON token
+    /// @param _TOS The address of the TOS token
+    /// @param _USDT The address of the USDT token
+    /// @param _USDC The address of the USDC token
+    /// @param _timeLimit The time limit between requests
+    /// @param _withdrawalAmount The amount of tokens to withdraw per request
     constructor(
         address _TON,
         address _TOS,
@@ -47,10 +58,8 @@ contract Faucet is owned {
         withdrawalAmount = _withdrawalAmount;
     }
 
-    /*
-    function requestAllTokens() external { }
-    */
-
+    /// @notice Requests a specific token from the faucet
+    /// @param token The token to request
     function requestToken(IERC20 token) external {
         require(token == TON || token == TOS || token == USDT || token == USDC, "Token not supported by faucet");
         require(token.balanceOf(address(this)) >= withdrawalAmount, "Not eunough balance in faucet");
@@ -63,14 +72,19 @@ contract Faucet is owned {
         emit WithdrawalRequest(msg.sender, token, withdrawalAmount);
     }
 
+    /// @notice Sets the time limit between requests
+    /// @param _timeLimit The new time limit
     function setTimeLimit(uint256 _timeLimit) external onlyOwner {
         timeLimit = _timeLimit;
     }
 
+    /// @notice Sets the withdrawal amount per request
+    /// @param _withdrawalAmount The new withdrawal amount
     function setWithdrawalAmount(uint256 _withdrawalAmount) external onlyOwner {
         withdrawalAmount = _withdrawalAmount;
     }
 
+    /// @notice Withdraws all tokens and Ether from the faucet
     function withdrawalAll() external onlyOwner {
         if (address(this).balance > 0) {
             payable(owner).transfer(address(this).balance);
@@ -82,11 +96,15 @@ contract Faucet is owned {
         _withdrawToken(USDC);
     }
 
+    /// @notice Transfers ownership of the contract to a new address
+    /// @param newOwner The address of the new owner
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
         owner = newOwner;
     }
 
+    /// @dev Internal function to withdraw a specific token
+    /// @param token The token to withdraw
     function _withdrawToken(IERC20 token) internal {
         uint256 balance = token.balanceOf(address(this));
         if (balance > 0) {
@@ -94,6 +112,7 @@ contract Faucet is owned {
         }
     }
 
+    /// @notice Fallback function to handle Ether deposits
     receive() external payable {
         emit Deposit(msg.sender, msg.value);
     }
