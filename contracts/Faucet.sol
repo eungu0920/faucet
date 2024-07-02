742f89dd62ca9ed21bbb71fb5f3aadc92b0c32d0
Gray <eungu0920@inu.ac.kr>
2024-07-02 13:45:21 +0900

    Enhance token management in Faucet contract

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -9,53 +9,55 @@ import "@openzepplin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Faucet is Ownable {
     using SafeERC20 for IERC20;
     uint256 public timeLimit;
-    uint256 public withdrawalAmount;
-    IERC20 public TON;
-    IERC20 public TOS;
-    IERC20 public USDT;
-    IERC20 public USDC;
+    IERC20[] public tokens;
 
+    mapping(IERC20 => uint256) public tokenAmounts;
     mapping(address => mapping(IERC20 => uint256)) public lastTokenRequestTime;
     
+    /// @notice Emitted when a withdrawal request is made
+    /// @param to The address to which the tokens are sent
+    /// @param token The token that is being withdrawn
+    /// @param amount The amount of tokens withdrawn
     event WithdrawalRequest(address indexed to, IERC20 token ,uint amount);
-    event Deposit(address indexed from, uint amount);
 
     /// @notice Initializes the contract with the given parameters
-    /// @param _TON The address of the TON token
-    /// @param _TOS The address of the TOS token
-    /// @param _USDT The address of the USDT token
-    /// @param _USDC The address of the USDC token
     /// @param _timeLimit The time limit between requests
-    /// @param _withdrawalAmount The amount of tokens to withdraw per request
-    constructor(
-        address _TON,
-        address _TOS,
-        address _USDT,
-        address _USDC,
-        uint256 _timeLimit,
-        uint256 _withdrawalAmount
-    ) {
-        TON = IERC20(_TON);
-        TOS = IERC20(_TOS);
-        USDT = IERC20(_USDT);
-        USDC = IERC20(_USDC);
-        
-        timeLimit = _timeLimit;
-        withdrawalAmount = _withdrawalAmount;
+    constructor(uint256 _timeLimit) {        
+        timeLimit = _timeLimit;        
     }
 
     /// @notice Requests a specific token from the faucet
     /// @param token The token to request
     function requestToken(IERC20 token) external {
-        require(token == TON || token == TOS || token == USDT || token == USDC, "Token not supported by faucet");
-        require(token.balanceOf(address(this)) >= withdrawalAmount, "Not eunough balance in faucet");
+        require(tokenAmounts[token] > 0, "Token not supported by faucet");
+        require(token.balanceOf(address(this)) >= tokenAmounts[token], "Not eunough balance in faucet");
 
         uint256 lastRequest = lastTokenRequestTime[msg.sender][token];
         require(block.timestamp >= lastRequest + timeLimit, "Time limit has not passed");
 
         lastTokenRequestTime[msg.sender][token] = block.timestamp;
-        token.safeTransfer(msg.sender, withdrawalAmount);
-        emit WithdrawalRequest(msg.sender, token, withdrawalAmount);
+        token.safeTransfer(msg.sender, tokenAmounts[token]);
+        emit WithdrawalRequest(msg.sender, token, tokenAmounts[token]);
+    }
+
+    /// @notice Sets the withdrawal amount for a specific token
+    /// @param token The token to set the amount for
+    /// @param amount The new withdrawal amount
+    function setTokenAmount(IERC20 token, uint256 amount) external onlyOwner {
+        tokenAmounts[token] = amount;
+
+        bool tokenExists = false;
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (tokens[i] == token) {
+                tokenExists = true;
+                break;
+            }
+        }
+
+        if (!tokenExists) {
+            tokens.push(token);    
+        }
     }
 
     /// @notice Sets the time limit between requests
@@ -64,29 +66,22 @@ contract Faucet is Ownable {
         timeLimit = _timeLimit;
     }
 
-    /// @notice Sets the withdrawal amount per request
-    /// @param _withdrawalAmount The new withdrawal amount
-    function setWithdrawalAmount(uint256 _withdrawalAmount) external onlyOwner {
-        withdrawalAmount = _withdrawalAmount;
-    }
-
     /// @notice Withdraws all tokens and Ether from the faucet
     function withdrawalAll() external onlyOwner {
         if (address(this).balance > 0) {
             payable(owner).transfer(address(this).balance);
         }
 
-        _withdrawToken(TON);
-        _withdrawToken(TOS);
-        _withdrawToken(USDT);
-        _withdrawToken(USDC);
+        for (uint256 i = 0; i < tokens.length; i++) {
+            _withdrawToken(tokens[i]);
+        }
     }
 
     /// @notice Transfers ownership of the contract to a new address
     /// @param newOwner The address of the new owner
-    function transferOwnership(address newOwner) public onlyOwner {
+    function transferOwnership(address newOwner) public override onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
-        owner = newOwner;
+        super.transferOwnership(newOwner);
     }
 
     /// @dev Internal function to withdraw a specific token
@@ -94,7 +89,7 @@ contract Faucet is Ownable {
     function _withdrawToken(IERC20 token) internal {
         uint256 balance = token.balanceOf(address(this));
         if (balance > 0) {
-            token.safeTransfer(msg.sender, balance);
+            token.safeTransfer(owner(), balance);
         }
     }
 
